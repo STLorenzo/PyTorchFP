90c1812930c1da41e5476ac222c648a13370fea9
StriderXR <pipematin@gmail.com>
2020-05-14 11:37:25 +0200

    13/05/2020 HalconU

--- src/nn_example.py
+++ src/nn_example.py
@@ -4,6 +4,7 @@ from torchvision import transforms, datasets
 import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
 train = datasets.MNIST('', train=True, download=True,
                        transform=transforms.Compose({
@@ -39,10 +40,48 @@ class Net(nn.Module):
 
 
 net = Net()
-print(net)
+# print(net)
 
 X = torch.rand((28, 28))
 # The -1 means that expected any size
+# We have to pass how many samples are going to be and their dim
 X = X.view(-1, 28 * 28)
 output = net(X)
-print(output)
+# print(output)
+
+# lr=0.001 <=> learning rate = 1e-3  
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+EPOCHS = 3
+
+for epoch in range(EPOCHS):
+    for data in trainset:
+        # data is a batch of featuresets and labels
+        X, y = data
+        net.zero_grad()
+        output = net(X.view(-1, 28 * 28))
+        loss = F.nll_loss(output, y)
+        # Back propagation
+        loss.backward()
+        # update weigths
+        optimizer.step()
+    print(loss)
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for data in trainset:
+        X, y = data
+        output = net(X.view(-1, 28 * 28))
+        for idx, i in enumerate(output):
+            if torch.argmax(i) == y[idx]:
+                correct += 1
+            total += 1
+
+print("Accuracy: ", round(correct/total, 3))
+plt.imshow(X[0].view(28, 28))
+plt.show()
+
+print(torch.argmax(net(X[0].view(-1, 28 * 28))[0]))
+
